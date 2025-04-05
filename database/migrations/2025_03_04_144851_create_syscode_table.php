f61b8e734ee2e6a981f34b30fe80992e05292a73
wendyyao <a151815058@gmail.com>
2025-04-05 09:27:00 +0800

    update syscode col "note"

--- database/migrations/2025_03_04_144851_create_syscode_table.php
+++ database/migrations/2025_03_04_144851_create_syscode_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('ParaSN')->comment('參數代碼');
             $table->string('Paramcode')->comment('參數名稱');
             $table->string('ParaNM')->comment('參數值');
-            $table->string('Note')->nullable();
+            $table->string('Note')->comment('備註')->nullable();
             $table->boolean('IsValid')->default(1);
             $table->string('Createuser')->comment('建立人員')->default('admin');
             $table->dateTime('CreateTime')->comment('建立時間')->default(now());
